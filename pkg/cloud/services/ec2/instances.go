ae9baa2b84bb829f564d7a0c2bb68df6846ddcbb
Jose Armesto <github@armesto.net>
2024-09-09 10:06:05 +0200

    Apply our customizations on top of upstream release-2.5 (#607)
    * Add Giant Swarm fork modifications
    
    * aws-cni-deleted-helm-managed-resources
    
    * Filter CNI subnets when creating EKS NodeGroup
    
    * Add non root volumes to AWSMachineTemplate
    
    * Support adding custom secondary VPC CIDR blocks in `AWSCluster` (backport) (#590)
    
    * S3 user data support for `AWSMachinePool` (#592)
    
    * Add GS workflow action
    
    ---------
    
    Co-authored-by: Andreas Sommer <andreas@giantswarm.io>
    Co-authored-by: calvix <vaclav@giantswarm.io>
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- pkg/cloud/services/ec2/instances.go
+++ pkg/cloud/services/ec2/instances.go
@@ -364,6 +364,13 @@ func (s *Service) findSubnet(scope *scope.MachineScope) (string, error) {
 					continue
 				}
 			}
+
+			tags := converters.TagsToMap(subnet.Tags)
+			if tags[infrav1.NameAWSSubnetAssociation] == infrav1.SecondarySubnetTagValue {
+				errMessage += fmt.Sprintf(" subnet %q belongs to a secondary CIDR block which won't be used to create instances.", *subnet.SubnetId)
+				continue
+			}
+
 			filtered = append(filtered, subnet)
 		}
 		// prefer a subnet in the cluster VPC if multiple match
@@ -380,7 +387,7 @@ func (s *Service) findSubnet(scope *scope.MachineScope) (string, error) {
 		return *filtered[0].SubnetId, nil
 	case failureDomain != nil:
 		if scope.AWSMachine.Spec.PublicIP != nil && *scope.AWSMachine.Spec.PublicIP {
-			subnets := s.scope.Subnets().FilterPublic().FilterByZone(*failureDomain)
+			subnets := s.scope.Subnets().FilterPublic().FilterNonCni().FilterByZone(*failureDomain)
 			if len(subnets) == 0 {
 				errMessage := fmt.Sprintf("failed to run machine %q with public IP, no public subnets available in availability zone %q",
 					scope.Name(), *failureDomain)
@@ -390,7 +397,7 @@ func (s *Service) findSubnet(scope *scope.MachineScope) (string, error) {
 			return subnets[0].GetResourceID(), nil
 		}
 
-		subnets := s.scope.Subnets().FilterPrivate().FilterByZone(*failureDomain)
+		subnets := s.scope.Subnets().FilterPrivate().FilterNonCni().FilterByZone(*failureDomain)
 		if len(subnets) == 0 {
 			errMessage := fmt.Sprintf("failed to run machine %q, no subnets available in availability zone %q",
 				scope.Name(), *failureDomain)
@@ -399,7 +406,7 @@ func (s *Service) findSubnet(scope *scope.MachineScope) (string, error) {
 		}
 		return subnets[0].GetResourceID(), nil
 	case scope.AWSMachine.Spec.PublicIP != nil && *scope.AWSMachine.Spec.PublicIP:
-		subnets := s.scope.Subnets().FilterPublic()
+		subnets := s.scope.Subnets().FilterPublic().FilterNonCni()
 		if len(subnets) == 0 {
 			errMessage := fmt.Sprintf("failed to run machine %q with public IP, no public subnets available", scope.Name())
 			record.Eventf(scope.AWSMachine, "FailedCreate", errMessage)
@@ -411,7 +418,7 @@ func (s *Service) findSubnet(scope *scope.MachineScope) (string, error) {
 		// with control plane machines.
 
 	default:
-		sns := s.scope.Subnets().FilterPrivate()
+		sns := s.scope.Subnets().FilterPrivate().FilterNonCni()
 		if len(sns) == 0 {
 			errMessage := fmt.Sprintf("failed to run machine %q, no subnets available", scope.Name())
 			record.Eventf(s.scope.InfraCluster(), "FailedCreateInstance", errMessage)
