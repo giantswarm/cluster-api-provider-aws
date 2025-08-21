cc8b0b5ad789fbee12b056cb47cec31a7e650d30
iuri aranda <iuri@giantswarm.io>
2025-08-21 13:23:16 +0200

    Check error first

--- exp/controllers/awsmachinepool_controller.go
+++ exp/controllers/awsmachinepool_controller.go
@@ -310,12 +310,12 @@ func (r *AWSMachinePoolReconciler) reconcileNormal(ctx context.Context, machineP
 		}
 
 		canProceed, err := r.isMachinePoolAllowedToUpgradeDueToControlPlaneVersionSkew(ctx, machinePoolScope)
+		if err != nil {
+			return true, nil, err
+		}
 		if !canProceed {
 			machinePoolScope.Info("blocking instance refresh due to control plane k8s version skew")
 			return false, nil, nil
-		}
-		if err != nil {
-			return true, nil, err
 		}
 
 		return asgsvc.CanStartASGInstanceRefresh(machinePoolScope)
