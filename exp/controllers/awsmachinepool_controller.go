c158bfd3e6fe0bc40499a198e15b9a95acb48503
Andreas Sommer <andreas@giantswarm.io>
2025-10-30 10:33:23 +0100

    Conditionally proceed if `isMachinePoolAllowedToUpgradeDueToControlPlaneVersionSkew` returns an error

--- exp/controllers/awsmachinepool_controller.go
+++ exp/controllers/awsmachinepool_controller.go
@@ -322,7 +322,7 @@ func (r *AWSMachinePoolReconciler) reconcileNormal(ctx context.Context, machineP
 
 		canProceed, err := r.isMachinePoolAllowedToUpgradeDueToControlPlaneVersionSkew(ctx, machinePoolScope)
 		if err != nil {
-			return true, nil
+			return canProceed, nil
 		}
 		if !canProceed {
 			machinePoolScope.Info("blocking instance refresh due to control plane k8s version skew")
